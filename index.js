a9806acc1f986b69b9b262f74464e321e26aa05f
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-02-02 20:07:37 +0530

    added form to collect vegetable details and added submit event handler

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("working");
+//console.log("working");
+
+document.getElementById("vegDetails").addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("submit clicked");
+});
