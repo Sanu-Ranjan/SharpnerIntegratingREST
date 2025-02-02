e1f736d7bc426cc366821b7e0ac99334b0f7fdda
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-02-02 20:55:40 +0530

    getting data on page load from crud crud and displaying in table

--- index.js
+++ index.js
@@ -3,6 +3,33 @@
 const baseURL =
   "https://crudcrud.com/api/a8a821303b054b8aab9da43fd6c39307/vegShop";
 
+document.addEventListener("DOMContentLoaded", () => {
+  const table = document.querySelector("#dataTable tbody");
+  axios.get(baseURL).then(
+    (res) => {
+      console.log(res.data);
+      let arrObj = res.data;
+
+      arrObj.forEach((element) => {
+        let row = document.createElement("tr");
+
+        row.innerHTML = `<td>${element.name}</td>
+        <td>${element.price}</td>
+        <td>${element.quantity}</td>
+        <td><input type="number" /></td>
+        <td>
+          <button >Buy</button>
+          <button onclick="deleteRow(this)">Delete</button>
+        </td>`;
+        table.appendChild(row);
+      });
+    },
+    (error) => {
+      console.log("error: could not get data :", error);
+    }
+  );
+});
+
 document.getElementById("vegDetails").addEventListener("submit", (event) => {
   event.preventDefault();
   let name = event.target.name.value;
