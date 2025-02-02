839880cff82fb898e31278d5b80003ebbf5ca449
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-02-02 20:41:06 +0530

    uploading to backend crud crud

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 //console.log("working");
 
+const baseURL =
+  "https://crudcrud.com/api/a8a821303b054b8aab9da43fd6c39307/vegShop";
+
 document.getElementById("vegDetails").addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log("submit clicked");
+  let name = event.target.name.value;
+  let price = event.target.price.value;
+  let quantity = event.target.quantity.value;
+
+  let payload = {
+    name: name,
+    price: price,
+    quantity: quantity,
+  };
+
+  axios.post(baseURL, payload).then(
+    (res) => {
+      console.log("item added");
+    },
+    (error) => {
+      console.log("error :", error);
+    }
+  );
+
+  //   console.log(name);
+  //   console.log(price);
+  //   console.log(quantity);
 });
 
 function deleteRow(button) {
